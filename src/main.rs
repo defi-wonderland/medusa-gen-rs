c0ffee21e9bd2821e5d3a7480e0198c3b0b330a5
drgorillamd <83670532+drgorillamd@users.noreply.github.com>
2025-02-01 21:54:57 +0100

    feat: tests

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
-mod cli;
-mod gen;
-mod types;
-
 use anyhow::Result;
 use clap::Parser;
-
-use crate::cli::Args;
-use crate::gen::generate_test_suite;
-use crate::types::ContractType;
+use medusa_gen::cli::Args;
 
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    generate_test_suite(&args)?;
+    medusa_gen::generate_test_suite(&args)?;
 
     Ok(())
 }
